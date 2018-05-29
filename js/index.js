1b8cb9c45f0c176cb1e7cea2ac77ca62037f9ce6
Tech-A27 <tech-a27@Tech-A27s-MacBook-Air.local>
2018-05-29 13:42:56 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function addTwo(num1, num2){
+  return num1+ num2;
+}
+
+function addThree(num1, num2, num3){
+  return num1 + num2 + num3;
+}
+
+function multiplyTwo(num1, num2) {
+  return num1 * num2;
+}
+
+function reverseWord(param1, param2){
+
+}
+
+
+// do not delete
+runTests();
